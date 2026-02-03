91bd7482b799638398c99b65b32df935fea4402a
Swathi14GR <goudarswathi@gmail.com>
2026-02-03 14:48:14 +0530

    a

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,43 +1,42 @@
-resource "azurerm_resource_group" "resource_group_name" {
+resource "azurerm_resource_group" "rg" {
   name     = var.resource_group_name
   location = var.location
 }
 
 module "network" {
   source   = "./modules/terraform-azure-network"
-  location           = var.location
-  rg                 = var.resource_group_name
-  vnet_name          = var.vnet_name
+  location = var.location
+  rg       = azurerm_resource_group.rg.name
+  vnet_name = var.vnet_name
 }
 
 module "loganalytics" {
-  source = "./modules/terraform-azure-loganalytics"
-  location           = var.location
-  rg                 = var.resource_group_name
-  law_name           = var.log_analytics_name
-  
+  source   = "./modules/terraform-azure-loganalytics"
+  location = var.location
+  rg       = azurerm_resource_group.rg.name
+  law_name = var.log_analytics_name
 }
 
 module "aks" {
-  source             = "./modules/terraform-azure-aks"
-  aks_subnet_id      = module.network.aks_subnet_id
-  log_analytics_id   = module.loganalytics.id
-  location           = var.location
-  rg                 = var.resource_group_name
-  cluster_name       = var.aks_cluster_name
+  source           = "./modules/terraform-azure-aks"
+  aks_subnet_id    = module.network.aks_subnet_id
+  log_analytics_id = module.loganalytics.id
+  location         = var.location
+  rg               = azurerm_resource_group.rg.name
+  cluster_name     = var.aks_cluster_name
 }
 
 module "keyvault" {
-  source         = "./modules/terraform-azure-keyvault"
-  location       = var.location
-  rg             = var.resource_group_name
-  key_vault_name = var.key_vault_name
-  kubelet_identity_object_id = module.aks.kubelet_identity_object_id 
+  source                     = "./modules/terraform-azure-keyvault"
+  location                   = var.location
+  rg                         = azurerm_resource_group.rg.name
+  key_vault_name             = var.key_vault_name
+  kubelet_identity_object_id = module.aks.kubelet_identity_object_id
 }
 
 module "acr" {
-  source      = "./modules/terraform-azure-acr"
-  location    = var.location
-  rg     = var.resource_group_name
-  acr_name    = var.acr_name
+  source   = "./modules/terraform-azure-acr"
+  location = var.location
+  rg       = azurerm_resource_group.rg.name
+  acr_name = var.acr_name
 }
