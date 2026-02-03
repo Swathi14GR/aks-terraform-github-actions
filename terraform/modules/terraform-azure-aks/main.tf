32d5e700476528dabf987c7bbb6a4329b7a24d42
Swathi14GR <goudarswathi@gmail.com>
2026-02-03 12:20:52 +0530

    first commit

--- terraform/modules/terraform-azure-aks/main.tf
+++ terraform/modules/terraform-azure-aks/main.tf
@@ -26,6 +26,6 @@ resource "azurerm_kubernetes_cluster" "aks" {
   }
 
   oms_agent {
-    log_analytics_workspace_id = azurerm_log_analytics_workspace.law.id
+    log_analytics_workspace_id = var.log_analytics_id
   }
 }
