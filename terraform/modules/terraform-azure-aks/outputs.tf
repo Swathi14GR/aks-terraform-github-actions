7ea8181041d645faf33cfdbec6d9df64a7e54560
Swathi14GR <goudarswathi@gmail.com>
2026-02-08 00:29:36 +0530

    prod tags

--- terraform/modules/terraform-azure-aks/outputs.tf
+++ terraform/modules/terraform-azure-aks/outputs.tf
@@ -1,10 +1,10 @@
-#output "kubelet_identity_object_id" {
- # value = azurerm_kubernetes_cluster.aks.kubelet_identity[0].object_id
-#}
+output "kubelet_identity_object_id" {
+  value = azurerm_kubernetes_cluster.aks.kubelet_identity[0].object_id
+}
 output "name" {
   value = azurerm_kubernetes_cluster.aks.name
 }
-output "kubelet_identity_object_id" {
-  description = "Object ID of the kubelet managed identity (used for ACR pulls)"
-  value = azurerm_kubernetes_cluster.aks.identity_profile[0].kubeletidentity[0].object_id
-}
+#output "kubelet_identity_object_id" {
+ # description = "Object ID of the kubelet managed identity (used for ACR pulls)"
+ # value = azurerm_kubernetes_cluster.aks.identity_profile[0].kubeletidentity[0].object_id
+#}
